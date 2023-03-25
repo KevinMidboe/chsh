9a27a5c4de3676351dd0574616e9b17dc26052e2
Kevin Midboe <kevin.midboe@gmail.com>
2023-03-25 15:02:41 +0100

    Add node@18 to default brew packages

--- deploy.sh
+++ deploy.sh
@@ -78,7 +78,7 @@ move_profile_file () {
 }
 
 install_packages_brew () {
-    declare -a packages=("cmake" "tree" "wget" "jq" "ripgrep" "watch" "tmux" "fish" "lua-language-server" "gh")
+    declare -a packages=("cmake" "tree" "wget" "jq" "ripgrep" "watch" "tmux" "fish" "lua-language-server" "node@18" "gh")
     echo "Installing ${#packages[@]} packages from brew"
 
     brew install --quiet "${packages[@]}"
